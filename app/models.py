a61448ad6528d0eaf3dafd7a34b0e60c080f7af4
Jheanel Brown <Jiinngerbread@gmail.com>
2021-08-03 20:45:32 -0500

    adding in stuff for the models- updates

--- app/models.py
+++ app/models.py
@@ -1,6 +1,12 @@
 from . import db
 from werkzeug.security import generate_password_hash
 
+
+affiliation = db.Table('affiliation', 
+    db.Column('uid', db.Integer, db.ForignKey('users.id'), primary_key=True),
+    db.Column('groupid', db.Integer, db.ForeignKey('groups.id'), primary_key=True )
+    )
+
 class Users(db.Model):
     __tablename__ = 'Users'
  
@@ -13,9 +19,10 @@ class Users(db.Model):
     profile_photo = db.Column(db.String(255))
     role = db.Column(db.String(255))
     created_at = db.Column(db.DateTime(timezone=True))
+    groups = db.relationship("Groups",  secondary=affiliation)
     
 
-    def __init__(self, first_name, last_name, username, password, email, role, profile_photo, created_at):
+    def __init__(self, first_name, last_name, username, password, email, role, profile_photo, created_at, groups):
         self.first_name = first_name
         self.last_name = last_name
         self.username = username
@@ -24,6 +31,7 @@ class Users(db.Model):
         self.profile_photo = profile_photo
         self.created_at = created_at
         self.role = role
+        self.groups=groups
 
     def is_authenticated(self):
         return True
@@ -59,7 +67,7 @@ class Events(db.Model):
     image = db.Column(db.String(255))
     website_url = db.Column(db.String(255))
     status = db.Column(db.String(255))
-    uid = db.Column(db.Integer, foreign_key = True)
+    uid = db.Column(db.Integer, db.ForeignKey('users.id'))
     created_at = db.Column(db.DateTime(timezone=True))
 
 
@@ -80,3 +88,65 @@ class Events(db.Model):
     def __repr__(self):
         return '<Events %r>' % (self.id)
 
+
+class Groups(db.Model):
+    __tablename__ = 'Groups'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1024))
+    admin = db.Column(db.Integer, db.ForeignKey('users.id'))
+    
+    def __init__(self, name, admin):
+        self.name = name
+        self.admin = admin
+        
+
+    def __repr__(self):
+        return '<Groups %r>' % (self.id)
+
+
+
+
+class Affiliate(db.Model):
+    __tablename__ = 'Affiliate'
+
+    userId = db.Column(db.String(1024), foreign_key=True)
+    groupId = db.Column(db.String(1024), foreign_key=True)
+    
+    def __init__(self, userId, groupId):
+        self.userId = userId
+        self.groupId = groupId
+        
+
+    def __repr__(self):
+        return '<Affiliate %r>' % (self.id)
+
+
+class Schedule(db.Model):
+    __tablename__ = 'Schedule'
+
+    eventId = db.Column(db.String(1024), foreign_key=True)
+    groupId = db.Column(db.String(1024), foreign_key=True)
+    
+    def __init__(self, eventId, groupId):
+        self.eventId = eventId
+        self.groupId = groupId
+        
+
+    def __repr__(self):
+        return '<Schedule %r>' % (self.id)
+
+
+class Submit(db.Model):
+    __tablename__ = 'Submit'
+
+    eventId = db.Column(db.String(1024), foreign_key=True)
+    userId = db.Column(db.String(1024), foreign_key=True)
+    
+    def __init__(self, eventId, userId):
+        self.eventId = eventId
+        self.userId = userId
+        
+
+    def __repr__(self):
+        return '<Submit %r>' % (self.id)
