05c5c390c784b0f4c0d3a342550a44b18d9c527b
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-08-04 12:57:31 -0500

    Merged with master

--- app/config.py
+++ app/config.py
@@ -5,16 +5,9 @@ import os
 class Config(object):
     """Base Config Object"""
     DEBUG = False
-<<<<<<< HEAD
-    SECRET_KEY = os.environ.get('SECRET_KEY') or ' '
-    SQLALCHEMY_DATABASE_URI = os.environ.get('MY_DATABASE_URL')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
-    # This is just here to suppress a warning from SQLAlchemy as it will soon be removed 
-=======
     SECRET_KEY = os.environ.get('SECRET_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('MY_DATABASE_URL') 
     SQLALCHEMY_TRACK_MODIFICATIONS = False # This is just here to suppress a warning from SQLAlchemy as it will soon be removed
->>>>>>> f50a7535eda6dce98df9de7155a7219b8f666162
     PROFILE_UPLOAD_FOLDER = os.environ.get('PROFILE_UPLOAD_FOLDER') or './profileUploads'
     EVENT_UPLOAD_FOLDER = os.environ.get('EVENT_UPLOAD_FOLDER') or './eventUploads'
     ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
