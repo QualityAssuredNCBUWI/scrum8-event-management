e064abd76ec2761d0e33aa053782b098daeb0438
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-08-03 23:06:41 -0500

    Deleted unrelated files

--- python-client-generated/python-client/swagger_client/api/__init__.py
+++
@@ -1,7 +0,0 @@
-from __future__ import absolute_import
-
-# flake8: noqa
-
-# import apis into api package
-from swagger_client.api.event_api import EventApi
-from swagger_client.api.user_api import UserApi
