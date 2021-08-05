93fdb5c446cf3f6d294972906033278d6934602d
Tai-Vaughn White <Isamutai200@gmail.com>
2021-08-04 21:09:11 -0500

    forms

--- app/forms.py
+++ app/forms.py
@@ -1,17 +1,24 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, FileField, IntegerField, DecimalField, SelectField
-from wtforms.validators import DataRequired, InputRequired,Email 
-from flask_wtf.file import FileField, FileRequired, FileAllowed
-
-class RegistrationForm(FlaskForm):
-    firstname = StringField('First Name' , validators=[InputRequired, DataRequired])
-    lastname = StringField('Last Name' , validators=[InputRequired, DataRequired])
-    email = StringField('Email', validators=[DataRequired(), Email()])
+from wtforms import SelectField, PasswordField, StringField, DateTimeField, TextAreaField
+from wtforms.validators import InputRequired, Email, Optional,EqualTo
 
 
-class PhotoForm(FlaskForm):
-    photo = FileField('Photo', validators=[
-        FileRequired(),
-        FileAllowed(['jpg', 'png', 'Images only!'])
-    ])
-    
+class LoginForm(FlaskForm):
+    email = StringField('Email' , validators=[Email()])
+    password = PasswordField('Password')
+
+class SignUpForm(FlaskForm):
+    first_name = StringField('First Name', validators=[InputRequired()])
+    last_name = StringField('Last Name', validators=[InputRequired])
+    email = StringField('Email', validators=[Email(),InputRequired()])
+    password = PasswordField('password', validators=[InputRequired, EqualTo('confirm',message='Must be the same as previous')])
+    confirm = PasswordField('Repeat Password')
+
+class CreateEvent(FlaskForm):
+    title = StringField('Title', validators=[InputRequired()])
+    start_date = DateTimeField('Start Date', validators=[InputRequired()])
+    end_date = DateTimeField ('End Date', validators=[InputRequired()])
+    description = TextAreaField('Description', validators=[InputRequired()])
+    venue = StringField('Venue', validators=[InputRequired()])
+    websiteurl = StringField('Web Site URL', [Optional()])
+    status = SelectField('Event Status',choices=[('Pending','Pending'),('Published','Published')],validators=[InputRequired()])
