ef702ab2bce60f280e7a1525c6bcf9ac8460d986
Phillipj1514 <phillipjunior95@gmail.com>
2021-08-05 13:32:56 -0500

    add the login feature

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,53 +1,53 @@
 
 
-let login_form = document.querySelector(".login-form form")
-let form_data = new FormData(login_form);
+// let login_form = document.querySelector(".login-form form")
+// let form_data = new FormData(login_form);
 
-let email = form_data.get("email")
-let password = form_data.get("password")
-let sub = {}
-sub.email = email;
-sub.password = password;
-let self = this;
-fetch("/api/auth/login", {
-    method: 'POST',
-    body: sub,
-    headers : {
-        'Accept': 'application/json',
-        "Content-Type":"application/json"
-    },
-    body: JSON.stringify(sub)
-    // headers: {'X-CSRFToken': token    },    credentials: 'same-origin'
-})    
-.then(function (response) {        
-    if(response.status == 404){
-    response.json().then((data) => {
-        router.push({ name: 'Login', params: { flashes: JSON.stringify(
-        [{
-            message: "Email or password is incorrect",
-            category: "danger"
-            }]
-        )}})
-    });
-    } else if (response.status == 200){
-    response.json().then((data) => {
-        window.location.replace('http://localhost:8079/')
-        localStorage.setItem('token', data.token)
-    });
-    }
-    })    
-.then(function (jsonResponse) {
-    // display a success message
-    console.log(jsonResponse);
+// let email = form_data.get("email")
+// let password = form_data.get("password")
+// let sub = {}
+// sub.email = email;
+// sub.password = password;
+// let self = this;
+// fetch("/api/auth/login", {
+//     method: 'POST',
+//     body: sub,
+//     headers : {
+//         'Accept': 'application/json',
+//         "Content-Type":"application/json"
+//     },
+//     body: JSON.stringify(sub)
+//     // headers: {'X-CSRFToken': token    },    credentials: 'same-origin'
+// })    
+// .then(function (response) {        
+//     if(response.status == 404){
+//     response.json().then((data) => {
+//         router.push({ name: 'Login', params: { flashes: JSON.stringify(
+//         [{
+//             message: "Email or password is incorrect",
+//             category: "danger"
+//             }]
+//         )}})
+//     });
+//     } else if (response.status == 200){
+//     response.json().then((data) => {
+//         window.location.replace('http://localhost:8079/')
+//         localStorage.setItem('token', data.token)
+//     });
+//     }
+//     })    
+// .then(function (jsonResponse) {
+//     // display a success message
+//     console.log(jsonResponse);
 
-    self.message=jsonResponse.message;
-    if(self.message == "Login successful") {
-        self.$router.push("/login")
-        localStorage.setItem('token', jsonResponse.token)
-    }
-    })    
-.catch(function (error) {
-    console.log(error);
+//     self.message=jsonResponse.message;
+//     if(self.message == "Login successful") {
+//         self.$router.push("/login")
+//         localStorage.setItem('token', jsonResponse.token)
+//     }
+//     })    
+// .catch(function (error) {
+//     console.log(error);
 
-    self.error=error.message;   
-});
+//     self.error=error.message;   
+// });
