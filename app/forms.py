a2f900372ae87804152985db2b348160d0905dcd
Nathaniel Bedassie <nathanielbedassie@gmail.com>
2021-08-05 12:38:58 -0500

    Added group field for event

--- app/forms.py
+++ app/forms.py
@@ -1,7 +1,7 @@
 from flask_wtf import FlaskForm
 from wtforms import SelectField, PasswordField, StringField, DateTimeField, TextAreaField,FormField
 from wtforms.fields.simple import FileField
-from wtforms.validators import InputRequired, Email, Optional, EqualTo
+from wtforms.validators import InputRequired, Email, Optional, EqualTo, DataRequired
 
 
 class LoginForm(FlaskForm):
@@ -24,6 +24,7 @@ class CreateEvent(FlaskForm):
     venue = StringField('Venue', validators=[InputRequired()])
     websiteurl = StringField('Web Site URL', [Optional()])
     status = SelectField('Event Status',choices=[('Pending','Pending'),('Published','Published')],validators=[InputRequired()])
+    group = SelectField('Group', description = 'Select a group', validators=[DataRequired()])
     images = FileField('Images', validators=[InputRequired()])
 
 class CreateGroup(FlaskForm):
